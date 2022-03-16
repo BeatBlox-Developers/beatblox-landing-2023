486a5ed0b5d9befcc4a18d920536d79adb53f1d3
mamboespacio <emadobao+1@gmail.com>
2022-03-16 17:04:15 -0300

    Fix Videos preloading

--- pages/_app.js
+++ pages/_app.js
@@ -3,35 +3,11 @@ import '../styles/custom.scss'
 import React, { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() =>{
-    setTimeout(() => setLoading(false), 3000);
-  })
-
-  if (!loading) {
-    return (
-      <React.Fragment>
-        <Component {...pageProps} />
-      </React.Fragment>
-    )
-  }
-  else {
-    return (
-      <React.Fragment>
-        <div className="loading h-100 w-100 d-flex justify-content-center">
-          <video
-            autoPlay={true}
-            controls={false}
-            loop
-            muted
-            src="/videos/loading.mp4"
-            type="video/mp4">
-          </video>
-        </div>
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default MyApp
