6aa4e4f59e34f1da3073ce64b4a13318a61f738a
mamboespacio <emadobao+1@gmail.com>
2022-03-16 17:22:25 -0300

    remove video loader

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,35 @@ import '../styles/custom.scss'
 import React, { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <React.Fragment>
-      <Component {...pageProps} />
-    </React.Fragment>
-  )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    setTimeout(() => setLoading(false), 3000);
+  })
+
+  if (!loading) {
+    return (
+      <React.Fragment>
+        <Component {...pageProps} />
+      </React.Fragment>
+    )
+  }
+  else {
+    return (
+      <React.Fragment>
+        <div className="loading h-100 w-100 d-flex justify-content-center">
+          <video
+            autoPlay={true}
+            controls={false}
+            loop
+            muted
+            src="/videos/loading.mp4"
+            type="video/mp4">
+          </video>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default MyApp
