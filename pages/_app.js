a1ef07cdccddcccb8838209d5e2a2ed3046670a9
mamboespacio <emadobao+1@gmail.com>
2022-05-03 17:33:03 -0300

    add Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import '../styles/custom.scss'
 import React, { useState, useEffect } from 'react';
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() =>{
     setTimeout(() => setLoading(false), 3000);
+    TagManager.initialize({ gtmId: 'GTM-KWGVZWP' });
   })
 
   if (!loading) {
