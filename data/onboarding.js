e94480553e8a19830f526a88b5b2ba95b079e94d
mamboespacio <emadobao+1@gmail.com>
2022-06-18 15:37:54 -0300

    Complete information for all sections

---
+++ data/onboarding.js
@@ -0,0 +1,70 @@
+export const playAtTheMetaverseData = {
+  video: '/videos/play.mp4',
+  video_mobile: '/videos/play-mobile.mp4',
+  image: '/images/onboarding/perform.png',
+  title: 'Play at the metaverse',
+  subtitle: 'Bring your sound into virtuality',
+  description: 'An open space for Musicians, Djs & Selectors to share their music in the metaverse.',
+  benefits: [
+    '+ Connect with your audience and meet new fans without physical barriers all around the globe.',
+    '+ New and unique show experiences.',
+    '+ A free way to jump on the metaverse trend.',
+    '+ Build your virtual identity.',
+    '+ Be a part of the next web evolution.',
+    '+ No cost, nor wallet required.',
+    '+ Extend your reach to a new generation of innovators.',
+    '+ Amplify your reach through our crossed channels platform.',
+  ],
+  fields: [
+    { label: 'Name:', name: 'name' },
+    { label: 'Email:', name: 'email' },
+    { label: 'RRSS:', name: 'rrss' },
+    { label: 'Preview of content:', name: 'preview' },
+    { label: 'About You:', name: 'about' },
+  ]
+};
+export const joinOurCatalogData = {
+  video: '/videos/join.mp4',
+  video_mobile: '/videos/join-mobile.mp4',
+  image: '/images/onboarding/join.png',
+  title: 'Join Our Catalog',
+  subtitle: 'Make your content unique and collectible',
+  description: 'Enable your audience to collect and share your art while accessing unique content and experiences through our platform.',
+  benefits: [
+    '+ Direct monetization from your songs.',
+    '+ 90% revenue for artists.',
+    '+ Perpetual income up to 10% from secondary sales.',
+    '+ Turn fans into partners.',
+    '+ Extend your reach to a new generation of innovators.',
+    '+ Amplify your reach through our crossed channels platform.',
+    '+ Jump into a low-cost & low carbon footprint network.',
+    '+ Access to our exclusive virtual stages.',
+  ],
+  fields: [
+    { label: 'Name:', name: 'name' },
+    { label: 'Email:', name: 'email' },
+    { label: 'RRSS:', name: 'rrss' },
+    { label: 'Preview of content:', name: 'preview' },
+  ]
+};
+export const haveYourSpotData = {
+  video: '/videos/huts.mp4',
+  video_mobile: '/videos/huts-mobile.mp4',
+  image: '/images/onboarding/huts.png',
+  title: 'Have your spot in our district',
+  subtitle: 'Build your presence in the Metaverse',
+  description: 'Take the spotlight within the fastest growing Virtual World.',
+  benefits: [
+    '+ Give your audience access to a more dynamic and interactive experience with your content. ',
+    '+ Display and sell NFTs, showcase products or just run an exhibition for your audience.',
+    '+ Add value to your audience and content by exploring the capabilities of Web 3.0 interactions and events. ',
+    '+ Extend your reach to a 650,000 MAU community of innovators.',
+    '+ Amplify your engagement through our crossed channels platform.',
+  ],
+  fields: [
+    { label: 'Name:', name: 'name' },
+    { label: 'Email:', name: 'email' },
+    { label: 'RRSS:', name: 'rrss' },
+    { label: 'Preview of content:', name: 'preview' },
+  ]
+};
