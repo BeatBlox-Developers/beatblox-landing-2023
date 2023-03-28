67764966ded5292f3a48ca4bc542732aa5db9104
mamboespacio <emadobao+1@gmail.com>
2023-03-28 19:57:48 -0300

    apply feedback v2

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,9 @@
 import { Navbar, Nav, Container, Modal } from "react-bootstrap";
-import Link from 'next/link';
 import { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
+import { Discord, Instagram, Twitter, XLg, Youtube, ArrowRight } from "react-bootstrap-icons";
+import Link, { LinkProps } from "next/link";
+import React, { PropsWithChildren } from "react";
 
 const Navigation = () => {
   const [navbar, setNavbar] = useState(false); 
@@ -18,26 +20,72 @@ const Navigation = () => {
   }
   const [showNav, setShowNav] = useState(false);
   const [loadCount, setLoadCount] = useState(0);
-  const goToSection = (section) => {
-    section.scrollIntoView()  
-    setShowNav(false);
-  }
+
+  const handleScroll = (e) => {
+    e.preventDefault();
+    const href = e.target.href;
+    const targetId = href.replace(/.*\#/, "");
+    // get the element by id and use scrollIntoView
+    const elem = document.getElementById(targetId);
+    window.scrollTo({
+      top: elem.getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+    setShowNav(false)
+  };
+
   const NavModal = (props) => {
-    return (
+    return(
       <Modal
         {...props}
         aria-labelledby="contained-modal-title-vcenter"
         centered
         className="navModal text-black text-center"
       >
-        <button className="" onClick={() => goToSection('#player')}>Player</button>
-        <button className="" onClick={() => goToSection('#marketplace')}>Marketplace</button>
-        <button className="" onClick={() => goToSection('#district')}>Virtual Music District</button>
-        <button className="" onClick={() => goToSection('#player')}>Join BeatBlox</button>
-        <button className="" onClick={() => goToSection('#player')}>Early Supporter NFT</button>
-
+        <div className="content d-flex justify-content-between align-items-start">
+          <div className="col-12 d-flex justify-content-between text-white">
+            <img src="/images/beatblox-logo-iso.svg"/>
+            <XLg style={{cursor: 'pointer'}}onClick={() => setShowNav(false)} size={24}></XLg>
+            
+          </div>
+          <div className="col-12">
+            <a onClick={(e)=> handleScroll(e)} href="#player" className="scrollLink pt-4 d-flex justify-content-between align-items-center">Player<ArrowRight/></a>
+            <a onClick={(e)=> handleScroll(e)} href="#marketplace" className="scrollLink pt-4 d-flex justify-content-between align-items-center">Marketplace<ArrowRight/></a>
+            <a onClick={(e)=> handleScroll(e)} href="#district" className="scrollLink pt-4 d-flex justify-content-between align-items-center">Virtual Music District<ArrowRight/></a>
+            <a onClick={(e)=> handleScroll(e)} href="#onboarding" className="scrollLink pt-4 d-flex justify-content-between align-items-center">Join BeatBlox<ArrowRight/></a>
+            <a onClick={(e)=> handleScroll(e)} href="" className="scrollLink pt-4 d-flex justify-content-between align-items-center">Early Supporter NFT<ArrowRight/></a>
+          </div>
+          <div className="col-12 text-white mt-3">
+            <div className="row">
+              <div className="col-12 text-start">
+               <a className="" href="#footer" onClick={(e)=> handleScroll(e)}>Contact</a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-start">
+                <a className=""href="">FAQs</a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-start">
+                <a className=""href="">Terms & Conditions</a>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-start">
+                <a className=""href="">Privacy Policy</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 text-white d-flex justify-content-start align-items-start mt-3">
+            <a className="social me-3"><Discord></Discord></a>
+            <a className="social me-3"><Twitter></Twitter></a>
+            <a className="social me-3"><Instagram></Instagram></a>
+          </div>
+        </div>
+        
       </Modal>
-    );
+    )
   }
 
   return(
@@ -52,22 +100,22 @@ const Navigation = () => {
             <img src="/images/logo.svg" alt=""/>
           </Navbar.Brand>
           <Nav className="ms-md-auto my-2 my-lg-0 d-flex align-items-center">
-            <Link href="#player">
+            <Link href="#player" onClick={(e)=> handleScroll(e)} >
               <a className="d-none d-md-flex py-3 px-4">
                 Player
               </a>
             </Link>
-            <Link href="#marketplace">
+            <Link href="#marketplace" onClick={(e)=> handleScroll(e)} >
               <a className="d-none d-md-flex py-3 px-4">
                 Marketplace
               </a>
             </Link>
-            <Link href="#district">
+            <Link href="#district" onClick={(e)=> handleScroll(e)} >
               <a className="d-none d-md-flex py-3 px-4">
                 Virtual Music District
               </a>
             </Link>
-            <Link href="#onboarding">
+            <Link href="#onboarding" onClick={(e)=> handleScroll(e)} >
               <a className="d-none d-md-flex py-3 px-4">
                 Join BeatBlox
               </a>
@@ -82,7 +130,6 @@ const Navigation = () => {
       </Navbar>
       <NavModal
         show={showNav}
-        onHide={() => setShowNav(false)}
       />
     </>
   )
