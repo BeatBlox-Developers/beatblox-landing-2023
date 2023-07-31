91a71d524928cf5221912ce732bf7eb2cebd35a7
nhussein11 <nicolashussein14@gmail.com>
2023-07-31 09:47:43 -0300

    fix: remove 'render district' content

--- data/district.js
+++ data/district.js
@@ -1,32 +1,32 @@
 export const sections = [
-    {
-        name: 'District',
-        poster: 'images/district/DistrictGral.jpg',
-        src: 'videos/district/DistritoRender.mp4'
-    },
-    {
-        name: 'Huts',
-        poster: 'images/district/Huts.jpg',
-        src: 'videos/district/huts.mp4'
-    },
-    {
-        name: 'Mountain Club',
-        poster: 'images/district/MountainClub.jpg',
-        src: 'videos/district/MountainClub.mp4'
-    },
-    {
-        name: 'Open Air Venue',
-        poster: 'images/district/OpenAirVenue.jpg',
-        src: 'videos/district/OpenAirVenue.mp4'
-    },
-    {
-        name: 'Record Store',
-        poster: 'images/district/RecordStore.jpg',
-        src: 'videos/district/RecordStore.mp4'
-    },
-    {
-        name: 'Render District',
-        poster: 'images/district/RenderDistrict.jpg',
-        src: 'videos/district/DistritoRender.mp4'
-    },
-]
+  {
+    name: "District",
+    poster: "images/district/DistrictGral.jpg",
+    src: "videos/district/DistritoRender.mp4",
+  },
+  {
+    name: "Huts",
+    poster: "images/district/Huts.jpg",
+    src: "videos/district/huts.mp4",
+  },
+  {
+    name: "Mountain Club",
+    poster: "images/district/MountainClub.jpg",
+    src: "videos/district/MountainClub.mp4",
+  },
+  {
+    name: "Open Air Venue",
+    poster: "images/district/OpenAirVenue.jpg",
+    src: "videos/district/OpenAirVenue.mp4",
+  },
+  {
+    name: "Record Store",
+    poster: "images/district/RecordStore.jpg",
+    src: "videos/district/RecordStore.mp4",
+  },
+  // {
+  //     name: 'Render District',
+  //     poster: 'images/district/RenderDistrict.jpg',
+  //     src: 'videos/district/DistritoRender.mp4'
+  // },
+];
