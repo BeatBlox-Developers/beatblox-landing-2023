e50433ad08284c6d3d438f239a579cd6cdfeaaa0
nhussein11 <nicolashussein14@gmail.com>
2023-08-09 11:49:45 -0300

    fix: roadmap animation

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { AnimationOnScroll } from "react-animation-on-scroll";
+import React, { useRef, useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { milestones as items } from "../data/roadmap";
 
 const Roadmap = () => {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("visible");
+          setIsVisible(true);
+        }
+      });
+    }, options);
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <section className="section-video py-5 d-flex align-items-center">
+    <section
+      ref={containerRef}
+      className="section-video py-5 d-flex align-items-center"
+    >
       <video
         autoPlay={true}
         controls={false}
@@ -25,17 +57,24 @@ const Roadmap = () => {
         </div>
         <div className="milestones row justify-content-center pt-3">
           {items.map((item, index) => {
+            const animationDelay = index * 0.2 + "s";
+
             return (
               <div
-                className="col-12 col-md-6 text-center my-1"
+                className={`col-12 col-md-6 text-center my-1 
+            ${isVisible ? "fade-in" : ""}`}
                 key={index}
-                style={{ width: "fit-content" }}
+                style={{
+                  width: "fit-content",
+                  animationDelay: animationDelay,
+                }}
               >
-                <AnimationOnScroll animateIn="animate__fadeInUp">
-                  <div className="btn btn-light rounded-pill p-3">
-                    {item.title}
-                  </div>
-                </AnimationOnScroll>
+                <div
+                  className="btn btn-light rounded-pill p-3"
+                  style={{ animationDelay: animationDelay }}
+                >
+                  {item.title}
+                </div>
               </div>
             );
           })}
@@ -44,4 +83,5 @@ const Roadmap = () => {
     </section>
   );
 };
+
 export default Roadmap;
