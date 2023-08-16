f695886be20840a0646352899feb6eb5d46f9344
nhussein11 <nicolashussein14@gmail.com>
2023-08-16 17:27:13 -0300

    fix: margin top to zero in perform in the metaversa page

--- components/Common.js
+++ components/Common.js
@@ -1,25 +1,29 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import Link from 'next/link';
+import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/router';
-import { useSwiper } from 'swiper/react';
-
+import { useRouter } from "next/router";
+import { useSwiper } from "swiper/react";
 
 export const Navigation = () => {
-  const [navbar, setNavbar] = useState(false); 
+  const [navbar, setNavbar] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", changeBackground);
   }, []);
   const changeBackground = () => {
     if (window.scrollY >= 80) {
       setNavbar(true);
-    }
-    else{
+    } else {
       setNavbar(false);
     }
-  }
-  return(
-    <Navbar variant="dark" fixed="top" expand="lg" className={ navbar ? 'active py-5 px-5 px-md-0' : 'py-5 px-5 px-md-0' }>
+  };
+  return (
+    <Navbar
+      variant="dark"
+      fixed="top"
+      expand="lg"
+      className={navbar ? "active py-5 px-5 px-md-0" : "py-5 px-5 px-md-0"}
+      style={{ marginTop: "0px" }}
+    >
       <Container>
         <Navbar.Brand href="/">
           <img src="/images/logo.svg" alt="" />
@@ -35,38 +39,54 @@ export const Navigation = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 export const Switcher = (props) => {
   const router = useRouter();
-  return(
+  return (
     <div className="row px-5 px-md-0 switcher d-none d-md-block">
-      <div className={ props.theme === 'black' ? 'text-black' : 'text-white' }>
+      <div className={props.theme === "black" ? "text-black" : "text-white"}>
         <Link href="perform-in-the-metaverse">
-          <a className={ router.pathname === '/onboarding/perform-in-the-metaverse' ? 'text-white text-decoration-underline' : '' }
-            rel="noreferrer">
-              Perform in the metaverse
+          <a
+            className={
+              router.pathname === "/onboarding/perform-in-the-metaverse"
+                ? "text-white text-decoration-underline"
+                : ""
+            }
+            rel="noreferrer"
+          >
+            Perform in the metaverse
           </a>
         </Link>
         <Link href="/onboarding/join-our-catalog">
-          <a className={
-            router.pathname === "/onboarding/join-our-catalog" ? "text-white text-decoration-underline" : "" }
-            rel="noreferrer">
-              Join our catalog
+          <a
+            className={
+              router.pathname === "/onboarding/join-our-catalog"
+                ? "text-white text-decoration-underline"
+                : ""
+            }
+            rel="noreferrer"
+          >
+            Join our catalog
           </a>
         </Link>
         <Link href="/onboarding/have-your-spot-in-our-district">
-          <a className={
-            router.pathname === "/onboarding/have-your-spot-in-our-district" ? "text-white text-decoration-underline" : "" }
-            rel="noreferrer">
-              Have your spot in our district
+          <a
+            className={
+              router.pathname === "/onboarding/have-your-spot-in-our-district"
+                ? "text-white text-decoration-underline"
+                : ""
+            }
+            rel="noreferrer"
+          >
+            Have your spot in our district
           </a>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const RenderVideo = (props) => {
   return (
@@ -77,16 +97,32 @@ export const RenderVideo = (props) => {
       muted
       playsInline
       src={props.video}
-      type="video/mp4"/>
+      type="video/mp4"
+    />
   );
-}
+};
 
-
-export const SwiperButtonPrev = ({children}) => {
+export const SwiperButtonPrev = ({ children }) => {
   const swiper = useSwiper();
-  return <button type="button" className="btn btn-lg btn-outline-light mt-3 rounded-pill" onClick={() => swiper.slidePrev()}>{children}</button>;
-}
-export const SwiperButtonNext = ({children}) => {
+  return (
+    <button
+      type="button"
+      className="btn btn-lg btn-outline-light mt-3 rounded-pill"
+      onClick={() => swiper.slidePrev()}
+    >
+      {children}
+    </button>
+  );
+};
+export const SwiperButtonNext = ({ children }) => {
   const swiper = useSwiper();
-  return <button type="button" className="btn btn-lg btn-outline-light mt-3 rounded-pill" onClick={() => swiper.slideNext()}>{children}</button>;
-}
+  return (
+    <button
+      type="button"
+      className="btn btn-lg btn-outline-light mt-3 rounded-pill"
+      onClick={() => swiper.slideNext()}
+    >
+      {children}
+    </button>
+  );
+};
