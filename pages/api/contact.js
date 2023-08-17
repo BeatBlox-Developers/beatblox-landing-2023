9a3d5c5063b2c861c5dcb07becc45fb7f9f416d8
nhussein11 <nicolashussein14@gmail.com>
2023-08-17 12:00:58 -0300

    fix: contact api from email updated

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -22,8 +22,7 @@ export default async (req, res) => {
   mg.messages
     .create("sandboxcf138aa4199440c69f096ddc314cb9b1.mailgun.org", {
       from: "sender@beatblox.org",
-      // to: ["onboarding@beatblox.org"],
-      to: ["nicolashussein14@gmail.com"],
+      to: ["onboarding@beatblox.org"],
       subject: `Onboarding Request | ${body.action}`,
       text: message,
       html: message.replace(/\r\n/g, "<br>"),
